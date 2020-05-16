da36c6e90afb5398e2901ebef4370b047c31116d
Alex Gunning <atm.gunning@gmail.com>
2020-05-17 01:23:56 +0200

    add vim and fish

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+alias aur-search="yay -Slq | fzf -m --preview 'yay -Si {1}'| xargs -ro yay -S"
+alias remove-package="yay -Qeq | fzf -m --preview 'yay -Qi {1}' | xargs -ro yay -Rs"
+alias cat="bat"
+#set -x brew ./linuxbrew
+set -gx PATH ~/.linuxbrew/bin $PATH
+#xmodmap ~/.Xmodmap
+starship init fish | source
+alias calc="insect"
+alias sysinfo="neofetch"
--- vim/.vimrc
+++ vim/.vimrc
@@ -7,3 +7,9 @@ nnoremap l k
 nnoremap ; l
 nnoremap K 5j
 nnoremap L 5k
+vnoremap j h
+vnoremap k j
+vnoremap l k
+vnoremap ; l
+vnoremap K 5j
+vnoremap L 5k
