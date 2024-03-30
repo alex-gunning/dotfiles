84ef00edac4d7a0dd3058c28574810288161f31e
Alex Gunning <dubbelodub1on1@gmail.com>
2024-03-30 22:05:46 +0100

    Add doom

---
+++ doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(prettier eglot eg prisma-ts-mode jtsx lsp-mode lsp-docker ac-ispell docker-compose-mode dockerfile-mode terraform-mode yaml-mode yaml typescript-mode tide rjsx-mode json-mode js2-refactor graphql-mode graphql-doc graphql format-all exec-path-from-shell @))
+ '(warning-suppress-log-types '((lsp-mode) (lsp-mode) (lsp-mode) (defvaralias)))
+ '(warning-suppress-types '((lsp-mode) (lsp-mode) (lsp-mode) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
